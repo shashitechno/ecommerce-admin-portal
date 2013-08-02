07575a63c720b3cd0fd6622da8bf2aa1dc0c663d
Braunson <braunson@gmail.com>
2013-08-02 14:46:38 -0400

    Update buyer_delete_profile.php
    Code correction, missing )

--- buyer_delete_profile.php
+++ buyer_delete_profile.php
@@ -1,12 +1,12 @@
 <?php
 include "db.php";
 
-$rs = mysql_query("SELECT email FROM trade_lead WHERE id = '".mysql_real_escape_string($_GET['id']."'");
+$rs = mysql_query("SELECT email FROM trade_lead WHERE id = '".mysql_real_escape_string($_GET['id'])."'");
 while( $r = mysql_fetch_array($rs) ){
   $var = $r[0];
 }
 
-mysql_query("DELETE FROM trade_lead WHERE id = '".mysql_real_escape_string($_GET['id']."'");
+mysql_query("DELETE FROM trade_lead WHERE id = '".mysql_real_escape_string($_GET['id'])."'");
 
 header("location:buyers.php?msg=The Buyer $var Deleted Sucessfully");
 ?>
